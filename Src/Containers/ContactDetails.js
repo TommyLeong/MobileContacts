592fe5c9b672d104ad016e2f7593d787c4f914d6
Tommy Leong <tommy.leong@activate.sg>
2020-12-04 22:28:10 +0800

    Create contacts and contactdetails container screen

---
+++ Src/Containers/ContactDetails.js
@@ -0,0 +1,18 @@
+import React, {} from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+const ContactDetails = ({}) => {
+
+  return (
+    <View>
+      <Text>
+          ContactsDetails
+      </Text>
+    </View>
+  );
+};
+
+export default ContactDetails;
