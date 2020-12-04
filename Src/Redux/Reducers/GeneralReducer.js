f6ac3787babad9f0634494a1a802bd84ece0c18d
Tommy Leong <tommy.leong@activate.sg>
2020-12-05 00:00:37 +0800

    - Create updateContactData redux action - append hardcoded data into redux action

--- Src/Redux/Reducers/GeneralReducer.js
+++ Src/Redux/Reducers/GeneralReducer.js
@@ -1,34 +1,44 @@
 import {
     GET_CONTACT_DATA,
     GET_CONTACT_DATA_SUCCESS,
-    GET_CONTACT_DATA_FAIL
+    GET_CONTACT_DATA_FAIL,
+    UPDATE_CONTACT_DATA,
   } from '../types';
-
+  
   const INITIAL_STATE = {
-      loading: false,
-      contactData: null,
-  }
-
+    loading: false,
+    contactData: null,
+  };
+  
   export default (state, action) => {
-    let currentState = state  
-    if(state == null) currentState = INITIAL_STATE
-
-    switch(action.type){
-        case GET_CONTACT_DATA:
-            return{
-                ...state,
-                loading:true,
-                contactData: null
-            }
-            
-        case GET_CONTACT_DATA_SUCCESS:
-            return{
-                ...state,
-                loading:true,
-                contactData: action.payload
-            }
-
-        default:
-            return currentState
+    let currentState = state;
+    if (state == null) {
+      currentState = INITIAL_STATE;
     }
-  }
+  
+    switch (action.type) {
+      case GET_CONTACT_DATA:
+        return {
+          ...currentState,
+          loading: true,
+          contactData: null,
+        };
+  
+      case GET_CONTACT_DATA_SUCCESS:
+        return {
+          ...currentState,
+          loading: false,
+          contactData: action.payload,
+        };
+  
+      case UPDATE_CONTACT_DATA:
+        return {
+          ...currentState,
+          contactData: action.payload,
+        };
+  
+      default:
+        return currentState;
+    }
+  };
+  
